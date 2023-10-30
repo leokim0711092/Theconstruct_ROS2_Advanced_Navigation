cb4e5a64eafd581be185dc8293498264260c5ceb
leokim0711092 <leokim10073@gmail.com>
2023-10-30 08:22:35 +0000

    Unit 2: 2.1 Navigate To Pose

--- nav2_new_features/setup.py
+++ nav2_new_features/setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='user',
-    maintainer_email='user@todo.todo',
+    maintainer_email='leokim10073@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
